bc046ca4b368effd9a4ec3a81577599684842731
germn <se0es@yandex.ru>
2016-06-28 21:18:04 +0300

    Use master for tarball.

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ if sys.version_info < (3, 5, 2):
 setup(
     name='aiogen',
     packages=['aiogen'],
-    version='0.1',
+    version='0.11',
     description='Asynchronous generators for asyncio',
     long_description='`Package Documentation <https://github.com/germn/aiogen/blob/master/README.md>`',
     author='Gerasimov Mikhail',
     author_email='Gerasimov-M-N@yandex.ru',
     url='https://github.com/germn/aiogen',
-    download_url='https://github.com/germn/aiogen/tarball/0.1',
+    download_url='https://github.com/germn/aiogen/tarball/master',
     license='MIT',
     keywords=[
         'asyncio',
