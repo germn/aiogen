8ca025ab961d0f7284927eb16ac988196968c41f
germn <se0es@yandex.ru>
2016-06-28 19:20:11 +0300

    Files to upload to PyPi.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='aiogen',
+    packages=['aiogen'],
+    version='0.1',
+    description='Asynchronous generators for asyncio',
+    author='Gerasimov Mikhail',
+    author_email='Gerasimov-M-N@yandex.ru',
+    url='https://github.com/germn/aiogen',
+    download_url='https://github.com/germn/aiogen/tarball/0.1',
+    keywords=['asyncio', 'asynchronous generator'],
+    classifiers=[],
+)
